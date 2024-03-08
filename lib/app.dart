44a734d2701ba92c2f9326cc0766d20024681413
Dapo <odapo138@gmail.com>
2024-03-08 19:27:20 +0100

    first commit

---
+++ lib/app.dart
@@ -0,0 +1,11 @@
+import 'package:expense_tracker/app_view.dart';
+import 'package:flutter/material.dart';
+
+class ExpenseApp extends StatelessWidget {
+  const ExpenseApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const ExpenseAppView();
+  }
+}
